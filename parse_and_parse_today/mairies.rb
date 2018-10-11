5c8165b050b01a2bee7c1107c15d43aa7e2f979e
christine <christinejaouen@hotmail.com>
2018-10-11 17:36:46 +0200

    2ème commit

--- parse_and_parse_today/mairies.rb
+++ parse_and_parse_today/mairies.rb
@@ -7,7 +7,12 @@ require 'open-uri'
 def get_the_email_of_a_townhal_from_its_webpage
   doc = Nokogiri::HTML(open("http://annuaire-des-mairies.com/95/vaureal.html"))
 #  //h3[@class = "r"]/a[@class = "l"]
-  doc.xpath(//body[@class = "txt-primary tr-last"]/td[@class = "txt_primary"]).each do |node|
-    puts node.text
-  end
+#doc.xpath('//h3[@class = "r"]/a[@class = "l"]').each do |node|
+#    doc.xpath(//main//section//tbody[@class = "txt-primary"]/td).each do |node|
+#      doc.xpath(//head/titre[@class = "txt-primary"]/td).each do |node|
+        a = doc.xpath('/html/body/div/main/section[2]/div/table/tbody/tr[4]/td[2]')
+        puts a.text
+
 end
+
+puts get_the_email_of_a_townhal_from_its_webpage
